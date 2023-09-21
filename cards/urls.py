604af6afea8a488b22fbc020efa1fd1433b94cce
Dylan Lim <dylan@Dylans-MacBook-Air-M1.local>
2023-09-22 01:00:59 +0800

    Add functionality for selecting number of cards

--- cards/urls.py
+++ cards/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'cards'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('play', views.play, name='play')
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,5 +1,41 @@
+import os
+import json
+import random
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.conf import settings
 
-# Create your views here.
+QUESTIONS_PATH = os.path.join(settings.STATIC_ROOT, 'cards/questions.json')
+
+
 def index(request):
+    if request.method == 'POST':
+        request.session['num_cards'] = request.POST['num_cards']
+        return HttpResponseRedirect(reverse('cards:play'))
+    
     return render(request, 'cards/index.html', {})
+
+
+def play(request):
+    num_cards = request.session.get('num_cards', None)
+    
+    with open(QUESTIONS_PATH, 'r') as json_file:
+        questions = json.load(json_file)
+
+    context = {
+        'num_cards': num_cards,
+        'questions': questions,
+        'filtered_questions': generate_questions(questions, int(num_cards)),
+    }
+
+    return render(request, 'cards/play.html', context)
+
+
+def generate_questions(questions, n):
+    random.shuffle(questions)
+    filtered_questions = {}
+    for stage in range(1,4):
+        stage_question = [q['question'] for q in questions if q['stage'] == stage]
+        filtered_questions['Stage ' + str(stage)] = stage_question[:n]
+    return filtered_questions
