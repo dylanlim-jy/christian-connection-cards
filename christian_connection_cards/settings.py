c711ae462bddafee5901afde391c5c635cff4654
Dylan Lim <dylan@Dylans-MacBook-Air-M1.local>
2023-09-23 04:46:56 +0800

    Unsafe debug mode on production to test render server error 500

--- christian_connection_cards/settings.py
+++ christian_connection_cards/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
